879686aa17cff16c5f2c969cc0a7e13bf353610b
wakeup37 <adianchris1@gmail.com>
2026-08-20 13:14:39 +0000

    Mise à jour des fichiers et de la structure

--- frontend/src/instrument.js
+++ frontend/src/instrument.js
@@ -3,12 +3,9 @@ import * as Sentry from "@sentry/react";
 Sentry.init({
   dsn: "https://4d9a8453ed9e09ce79603032a9d1d8b4@o4511943155187712.ingest.de.sentry.io/4511943162921040",
   integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration()
+    Sentry.browserTracingIntegration()
   ],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/okepdydyxgsfywoknhqq\.supabase\.co/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
   enableLogs: true
 });
